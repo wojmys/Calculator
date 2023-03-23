b1f71fd87e368f21c04605557da568a29398adf9
Wojciech Mysliwiec <wojmys@gmail.com>
2023-03-23 11:46:12 +0100

    add 2 methods: addiction, substraction

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -1,4 +1,28 @@
 package com.kodilla.kalkulator;
 
 public class Calculator {
+    public static void main(String[] args) {
+
+
+        System.out.println(addition(0.1,-2,3));
+        System.out.println(subtraction(5,3,-0.5));
+
+    }
+
+    static double addition(double...value){
+        double result=0;
+        for (int i=0;i<value.length;i++){
+            result+=value[i];
+        }
+        return result;
+    }
+    static double subtraction(double...value){
+        double result=0;
+        for (int i=0;i<value.length;i++){
+            result-=value[i];
+        }
+        return result;
+    }
+
+
 }
