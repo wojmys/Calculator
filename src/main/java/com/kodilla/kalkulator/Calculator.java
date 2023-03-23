bee4df84b4daf10fc25ff4de80eee6a23ea76282
Wojciech Mysliwiec <wojmys@gmail.com>
2023-03-23 11:53:23 +0100

    upgrade of methods: addition, substraction

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -1,28 +1,30 @@
 package com.kodilla.kalkulator;
 
 public class Calculator {
+
+    public double addition(double... value) {
+        double result = 0;
+        for (int i = 0; i < value.length; i++) {
+            result += value[i];
+        }
+        return result;
+    }
+
+    public double subtraction(double... value) {
+        double result = 0;
+        for (int i = 0; i < value.length; i++) {
+            result -= value[i];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
 
-        System.out.println(addition(0.1,-2,3));
-        System.out.println(subtraction(5,3,-0.5));
+        System.out.println(calculator.addition(1, 5, -0.5));
+        System.out.println(calculator.subtraction(10, -0.2));
 
     }
-
-    static double addition(double...value){
-        double result=0;
-        for (int i=0;i<value.length;i++){
-            result+=value[i];
-        }
-        return result;
-    }
-    static double subtraction(double...value){
-        double result=0;
-        for (int i=0;i<value.length;i++){
-            result-=value[i];
-        }
-        return result;
-    }
-
 
 }
